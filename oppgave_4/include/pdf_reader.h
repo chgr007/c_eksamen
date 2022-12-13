b4550f41be5ddf702393f3baacded5d05dea582e
chgr007 <chgr007@student.kristiania.no>
2022-12-13 19:56:41 +0100

    Burde ikke være så langt unna noe fungerende nå tror jeg

--- oppgave_4/include/pdf_reader.h
+++ oppgave_4/include/pdf_reader.h
@@ -8,12 +8,13 @@ typedef struct _PDF_BYTE_BUFFER {
     BYTE byBuffer[4096];
     int iFileSize;
     int iaFileBytesAnalyzed[16];
+    int iErrorCode;
     sem_t semWaitForBuffer;
     sem_t semWaitForProcessing;
 
 } PDF_BYTE_BUFFER;
 
-int PdfReader(char *pszFileName, PDF_BYTE_BUFFER *structPdfByteBuffer);
-int PdfAnalyzer(PDF_BYTE_BUFFER *pdfByteBuffer);
+void *PdfReader(char *pszFileName, PDF_BYTE_BUFFER *structPdfByteBuffer);
+void *PdfAnalyzer(PDF_BYTE_BUFFER *pdfByteBuffer);
 
 #endif //C_EKSAMEN_PDF_READER_H
--- oppgave_4/main.c
+++ oppgave_4/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
-
+#include <string.h>
 #include "include/pdf_reader.h"
 
 /* Entry point for a program which analyzes the byte-composition of a PDF-document.
@@ -12,7 +12,7 @@
  * */
 int main(int iArgC, char *pszArgV[]) {
     pthread_t threadReader, threadAnalyzer;
-    PDF_BYTE_BUFFER *pdfByteBuffer = (PDF_BYTE_BUFFER*) malloc(sizeof (PDF_BYTE_BUFFER));
+    PDF_BYTE_BUFFER *pdfByteBuffer = (PDF_BYTE_BUFFER *) malloc(sizeof(PDF_BYTE_BUFFER));
 
     /* Check length of arguments and return values before going any further */
     if (iArgC < 2) {
@@ -20,11 +20,11 @@ int main(int iArgC, char *pszArgV[]) {
         return 1;
     }
     if (pdfByteBuffer == NULL) {
-        printf("Failed to allocate %lu bytes of memory for pdfByteBuffer\n", sizeof (PDF_BYTE_BUFFER));
+        printf("Failed to allocate %lu bytes of memory for pdfByteBuffer\n", sizeof(PDF_BYTE_BUFFER));
         return 1;
     }
 
-    if (pthread_create(&threadReader, NULL, (BYTE*) PdfReader, pszArgV[1]) != 0) {
+    if (pthread_create(&threadReader, NULL, (BYTE *) PdfReader, pszArgV[1]) != 0) {
         printf("Error when creating thread\n");
         return 1;
     }
@@ -36,48 +36,67 @@ int main(int iArgC, char *pszArgV[]) {
     sem_init(&pdfByteBuffer->semWaitForBuffer, 0, 0);
     sem_init(&pdfByteBuffer->semWaitForProcessing, 0, 0);
 
-    PDF_BYTE_BUFFER *structPdfByteBuffer = (PDF_BYTE_BUFFER*) malloc(sizeof (PDF_BYTE_BUFFER));
+    PDF_BYTE_BUFFER *structPdfByteBuffer = (PDF_BYTE_BUFFER *) malloc(sizeof(PDF_BYTE_BUFFER));
 
-    /* C89 doesn't have the void pointer, so I'll stick with the classic BYTE (unsigned char), if the compiler lets me. */
-    pthread_create(&threadReader, NULL, (BYTE*) PdfReader, pszArgV[1]); ;
-    pthread_create(&threadAnalyzer, NULL, (BYTE *) PdfAnalyzer, NULL);
+    /* C89 doesn't have the void pointer, but I have to use it here according to the documentation */
+    pthread_create(&threadReader, NULL, (void *) PdfReader, pszArgV[1]);;
+    pthread_create(&threadAnalyzer, NULL, (void *) PdfAnalyzer, NULL);
 
     return 0;
 }
 
-int PdfReader(char *pszFileName, PDF_BYTE_BUFFER *structPdfByteBuffer) {
+void *PdfReader(char *pszFileName, PDF_BYTE_BUFFER *structPdfByteBuffer) {
     long lFileBytes;
-    char *sFileContent;
-    FILE *fpFile = fopen("test.pdf", "r");
-
+    char *szFileContent;
+    int iBytesRead = 0;
 
     // open a file called text_to_read.txt, return 1 if there's an error opening the file
     FILE *fdFile = fopen(pszFileName, "r");
     if (fdFile == NULL) {
         printf("Error opening file");
-        return 1;
+        return (void *) ERROR;
     }
 
     // Rewind the cursor to the end of the file, count it's size and rewind it again. Then allocate
-    // memory for the file content
+    // memory for the file content. TODO: Check for errors on fseek and ftell!
     fseek(fdFile, 0L, SEEK_END);
     lFileBytes = ftell(fdFile);
     fseek(fdFile, 0L, SEEK_SET);
-    sFileContent = (char *) malloc(lFileBytes * sizeof(char *));
+    szFileContent = (char *) malloc(lFileBytes * sizeof(char));
+    // Read the file content into the allocated memory. TODO: Check for errors on fread!
+    // I could probably just do this in one operation. So that's a nother TODO. Now my plan is to read the file, then send the bytes in chunks.
+    fread(szFileContent, sizeof(char), lFileBytes, fdFile);
 
-    // Read the file content into the allocated memory
-    int iReadSuccess = fread(sFileContent, sizeof(char), lFileBytes, fdFile);
-    if(ferror(fdFile) && !feof(fdFile)) {
-        printf("Error reading the file!\n");
-        return 1;
+
+    int i, iBufferIndex = 0;
+    if (!ferror(fdFile) && feof(fdFile)) {
+        for (i = 0; i < lFileBytes; i++) {
+            /* i % 4095 makes the index not go out of bounds (4096 would make it go off by one!) */
+            iBufferIndex = i % 4095;
+            structPdfByteBuffer->byBuffer[iBufferIndex] = (BYTE) szFileContent[i];
+            iBytesRead++;
+            if (i != 0 && iBufferIndex == 0) {
+                structPdfByteBuffer->iFileSize = iBytesRead;
+                sem_post(&structPdfByteBuffer->semWaitForBuffer);
+                sem_wait(&structPdfByteBuffer->semWaitForProcessing);
+            }
+        }
+    } else {
+        fclose(fdFile);
+        return ERROR;
     }
+
+
     // Close the file and free the memory
     fclose(fdFile);
 
-    return 0;
+    return (void *) OK;
 }
-int PdfAnalyzer(PDF_BYTE_BUFFER *pdfByteBuffer) {
 
+void *PdfAnalyzer(PDF_BYTE_BUFFER *pdfByteBuffer) {
     sem_wait(&pdfByteBuffer->semWaitForBuffer);
-    return 0;
+    sem_post(&pdfByteBuffer->semWaitForProcessing);
+    printf("File size: %d\n", pdfByteBuffer->iFileSize);
+    printf("Bytes in buffer: %s\n", pdfByteBuffer->byBuffer);
+    return (void *) OK;
 }
