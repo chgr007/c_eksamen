edfbfdbdc5a7692ab2fee1b9c49c37e39f0139ce
chgr007 <chgr007@student.kristiania.no>
2022-12-13 14:20:26 +0100

    laget funksjoner og tilhørende tråder

--- oppgave_4/include/pdf_reader.h
+++ oppgave_4/include/pdf_reader.h
@@ -6,5 +6,12 @@ typedef struct _PDF_BYTE_BUFFER {
     BYTE byBuffer[4096];
     int iFileSize;
     int iaFileBytesAnalyzed[16];
+    sem_t semWaitForBuffer;
+    sem_t semWaitForProcessing;
+
 } PDF_BYTE_BUFFER;
+
+int PdfReader(char *pszFileName);
+int PdfAnalyzer(PDF_BYTE_BUFFER *pdfByteBuffer);
+
 #endif //C_EKSAMEN_PDF_READER_H
--- oppgave_4/main.c
+++ oppgave_4/main.c
@@ -1,21 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
 #include "include/pdf_reader.h"
 
 int main(int iArgC, char *pszArgV[]) {
-    long lFileBytes;
-    char *sFileContent;
-    FILE *fpFile = fopen("test.pdf", "r");
-
-    PDF_BYTE_BUFFER *pdfByteBuffer = (PDF_BYTE_BUFFER*) malloc(sizeof (PDF_BYTE_BUFFER));
 
     if (iArgC < 2) {
         printf("Please specify a file to read, ex. %s filename\n", pszArgV[0]);
         return 1;
     }
 
+    pthread_t threadReader, threadAnalyzer;
+
+    if (pthread_create(&threadReader, NULL, (void *) PdfReader, pszArgV[1]) != 0) {
+        printf("Error when creating thread\n");
+        return 1;
+    }
+    if (pthread_create(&threadAnalyzer, NULL, (void *) PdfAnalyzer, NULL) != 0) {
+        printf("Error when creating thread\n");
+        return 1;
+    }
+
+    return 0;
+}
+
+int PdfReader(char *pszFileName) {
+    long lFileBytes;
+    char *sFileContent;
+    FILE *fpFile = fopen("test.pdf", "r");
+
+    PDF_BYTE_BUFFER *pdfByteBuffer = (PDF_BYTE_BUFFER*) malloc(sizeof (PDF_BYTE_BUFFER));
+
+
+
     // open a file called text_to_read.txt, return 1 if there's an error opening the file
-    FILE *fdFile = fopen(pszArgV[1], "r");
+    FILE *fdFile = fopen(pszFileName, "r");
     if (fdFile == NULL) {
         printf("Error opening file");
         return 1;
@@ -37,5 +58,8 @@ int main(int iArgC, char *pszArgV[]) {
     // Close the file and free the memory
     fclose(fdFile);
 
+    return 0;
+}
+int PdfAnalyzer(PDF_BYTE_BUFFER *pdfByteBuffer) {
     return 0;
 }
