ed07d2ab5a3168631cf3eb3c2e50280b04695791
chgr007 <chgr007@student.kristiania.no>
2022-12-20 15:48:33 +0100

    Refactor and commenting ok on oppg2

--- oppgave_2/include/main.h
+++ oppgave_2/include/main.h
@@ -1,4 +1,38 @@
 #ifndef C_EKSAMEN_MAIN_H
 #define C_EKSAMEN_MAIN_H
+#define INPUT_FILE "./text_to_read.txt"
+#define OUTPUT_FILE "./text_to_write.out.txt"
 
+
+/*
+ * Takes a string of numbers, and converts it to a string of hex values
+ * First param is the input string, second param is the output string
+ *
+ * Returns 1 if successful, 0 if not
+ */
+
+int ConvertStringToHex(char *szInputString, char *szOutputString);
+
+/*
+ * Takes a file pointer as argument and returns
+ * the file size in bytes
+ *
+ * returns -1 on error
+ * */
+unsigned long GetFileSize(FILE *pFile);
+
+/*
+ * Opens a file, and reads the content into a string
+ *
+ * First param is the path, the second is a string to store the content in
+ * On success it returns the length of the string, on failure it returns 0
+ *
+ */
+unsigned long GetFileContent(FILE *fdFile, char *szFileContent, unsigned long ulFileSize);
+/*
+ * Takes a file as parameter, and writes its content as HEX in a new file.
+ *
+ * The file input and output is defined in the header file as INPUT_FILE and OUTPUT_FILE
+ * */
+int ConvertToHex(FILE *fdFile);
 #endif //C_EKSAMEN_MAIN_H
--- oppgave_2/main.c
+++ oppgave_2/main.c
@@ -1,64 +1,127 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define INPUT_FILE "./text_to_read.txt"
-#define OUTPUT_FILE "./text_to_write.out.txt"
+#include "include/main.h"
 
-// Kode hentet fra min egen oppg. 6
+int ConvertStringToHex(char *szInputString, char *szOutputString) {
+    unsigned long i = 0, ulStrLen = 0;
+    int iRetVal = 1;
+    /* Loop through each char, print it to hex with sprintfs %X option */
+    ulStrLen = strlen(szInputString) - 1;
+    char *szTemp = (char *) malloc(24);
+    memset(szTemp, 0, 24);
 
-// TODO:
-//  - Sjekke for memory leaks
-//  - Flytte ting i metoder
-int main() {
-    long lFileBytes;
-    char *sFileContent;
+    if (szTemp != NULL) {
+        if (szInputString != NULL) {
+            for (i = 0; i < ulStrLen; i++) {
+                if (szInputString[i] == '\0') {
+                    break;
+                }
+                memset(szTemp, 0, 20);
+                sprintf((char *) szTemp, "%02X", szInputString[i]);
+                strcat(szOutputString, szTemp);
+            }
+        } else {
+            printf("Input string is NULL\n");
+            iRetVal = 0;
+        }
+        free(szTemp);
+    }
 
-    // open a file called text_to_read.txt, return 1 if there's an error opening the file
+    return iRetVal;
+}
+
+
+unsigned long GetFileSize(FILE *pFile) {
+    unsigned long ulFileSize;
+    fseek(pFile, 0, SEEK_END);
+    ulFileSize = ftell(pFile);
+    fseek(pFile, 0, SEEK_SET);
+    return ulFileSize;
+}
+
+
+
+unsigned long GetFileContent(FILE *fdFile, char *szFileContent, unsigned long ulFileSize) {
+    unsigned long iReturnVal = 1;
+
+    printf("Allocated %lu bytes for file content\n", ulFileSize);
+
+    fread(szFileContent, sizeof(char), ulFileSize, fdFile);
+    // Just to be sure it's null terminated
+    szFileContent[ulFileSize] = '\0';
+    if (ferror(fdFile) != 0) {
+        printf("ERROR reading file\n");
+        iReturnVal = 0;
+    }
+
+    printf("Read %lu bytes from file\n", ulFileSize);
+
+    return iReturnVal;
+}
+
+
+int ConvertToHex(FILE *fdFile) {
+    char *szFileContent;
+    size_t lFileBytes;
+    int iReturnVal = 0;
+
+    lFileBytes = GetFileSize(fdFile);
+
+    if (lFileBytes > 0) {
+        szFileContent = (char *) malloc(lFileBytes * sizeof(char) + 2);
+        if (szFileContent == NULL) {
+            printf("ERROR allocating space to: %s\n", INPUT_FILE);
+        } else {
+            memset(szFileContent, 0, lFileBytes * sizeof(char) + 2);
+            if (GetFileContent(fdFile, szFileContent, lFileBytes)) {
+                // Need a bigger allocation for the hex representation
+                char *szAsciiFormatedText = (char *) malloc((lFileBytes * sizeof(char) * 5));
+                memset(szAsciiFormatedText, 0, (lFileBytes * sizeof(char) * 5));
+
+                if (szAsciiFormatedText != NULL) {
+                    if (ConvertStringToHex(szFileContent, szAsciiFormatedText)) {
+                        // Write the hex text to a file
+                        FILE *fdOutputFile = fopen(OUTPUT_FILE, "w");
+                        if (fdOutputFile == NULL) {
+                            printf("ERROR opening file %s\n", OUTPUT_FILE);
+                            iReturnVal = 1;
+                        }
+                        fwrite(szAsciiFormatedText, sizeof(char), strlen(szAsciiFormatedText), fdOutputFile);
+                        fclose(fdOutputFile);
+                    } else {
+                        printf("ERROR converting string to hex\n");
+                        iReturnVal = 1;
+                    }
+                    free(szAsciiFormatedText);
+                } else {
+                    printf("ERROR allocating memory\n");
+                    iReturnVal = 1;
+                }
+            } else {
+                printf("ERROR reading file\n");
+                iReturnVal = 1;
+            }
+        }
+        free(szFileContent);
+    } else {
+        printf("ERROR getting file content\n");
+        iReturnVal = 1;
+    }
+    return iReturnVal;
+}
+
+int main(int iArgC, char *pszArgV[]) {
+    int iRetVal = 0;
     FILE *fdFile = fopen(INPUT_FILE, "r");
     if (fdFile == NULL) {
-        printf("Error opening file");
-        return 1;
+        printf("ERROR opening file\n");
+        return 0;
     }
-    // Rewind the cursor to the end of the file, count it's size and rewind it again. Then allocate
-    // memory for the file content
-    fseek(fdFile, 0L, SEEK_END);
-    lFileBytes = ftell(fdFile);
-    fseek(fdFile, 0L, SEEK_SET);
-    sFileContent = (char *) malloc(lFileBytes * sizeof(char *));
-
-    // Read the file content into the allocated memory
-    fread(sFileContent, sizeof(char), lFileBytes, fdFile);
-    // Close the file and free the memory
+    if (ConvertToHex(fdFile)) {
+        printf("ERROR converting to hex\n");
+        iRetVal = 1;
+    }
     fclose(fdFile);
-    // Print the file content
-    printf("%s", sFileContent);
-    puts("\n");
-
-    int i;
-    // Allocate twice the size of the file content, since we're converting it to hex
-    char *szAsciiFormatedText = (char *) malloc((lFileBytes * sizeof(char *) * 2));
-    bzero(szAsciiFormatedText, (lFileBytes * sizeof(char *) * 2));
-
-    // Loop through the file content and convert it to hex
-    for (i = 0; i < strlen(sFileContent) - 1; i++) {
-        int iCurrentChar = *(sFileContent + i);
-        char *szHexValue = (char *) malloc(2 * sizeof(char));
-        sprintf(szHexValue, "%X", iCurrentChar);
-        printf("%c, %d, 0x%X", iCurrentChar, (char) *(sFileContent + i), iCurrentChar);
-        strncat(szAsciiFormatedText, szHexValue, 2);
-        puts("\n");
-    }
-    printf("Hex text: %s", szAsciiFormatedText);
-
-    // Write the hex text to a file
-    FILE *fdOutputFile = fopen(OUTPUT_FILE, "w");
-    if (fdOutputFile == NULL) {
-        printf("Error opening file");
-        return 1;
-    }
-    fwrite(szAsciiFormatedText, sizeof(char), strlen(szAsciiFormatedText), fdOutputFile);
-    fclose(fdOutputFile);
-
-    free(szAsciiFormatedText);
-    return 0;
+    return iRetVal;
 }
