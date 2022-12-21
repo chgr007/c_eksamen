1e66450cee095d711b18467f65899a2983484863
chgr007 <chgr007@student.kristiania.no>
2022-12-21 16:46:08 +0100

    Oppgave7 OK

---
+++ oppgave_7/testfile.c
@@ -0,0 +1,13 @@
+int main(int iArgC, char *pszArgV[]) {
+
+	for (int i = 0, j = 9, k = 8; i < 10; i++, j++) {
+                printf("Hello world");
+        		for (int a = 1; a < 2; a++) {
+                        	printf("Hello world");
+               			for (int b = 1; b < 2; b++) {
+                                	printf("Hello world");
+                       		 }
+                	}
+        }
+	return 0;
+}
