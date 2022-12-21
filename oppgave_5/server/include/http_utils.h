49813bbf89a966ac7224ccdd046140dbcb51a62c
chgr007 <chgr007@student.kristiania.no>
2022-12-21 12:24:09 +0100

    refactor server.c

--- oppgave_5/server/include/http_utils.h
+++ oppgave_5/server/include/http_utils.h
@@ -43,27 +43,91 @@ typedef struct _HTTP_REQUEST {
     struct HTTP_REQUEST_HEADERS *structHeaders;
 } HTTP_REQUEST;
 
+/*
+ * Takes an HTTP_REQUEST and an HTTP_RESPONSE as params
+ *
+ * Sets the content-type in the HTTP_RESPONSE based on the file extension in the HTTP_REQUEST
+ *
+ * Setting content type according to file format
+ * */
+static int SetResponseContentType(HTTP_REQUEST *structRequest, HTTP_RESPONSE *structResponse);
+
+/* The function takes in a request line and returns the file/path to the szFileName pointer
+ * The requested path / file resides in the middle of the request line with space as a delimiter.
+ *
+ * Returns OK on success and ERROR on failure.
+ * */
 static int ParseRequestLine(char *szRequestLine, HTTP_REQUEST *structRequest);
 
+/*
+ * Takes a client socket and HTTP_REQUEST as params
+ *
+ * Sets the headers and request line in the HTTP_REQUEST
+ * Returns OK if successful, ERROR if not
+ * */
 static int ParseRequestHeaders(int sockFd, HTTP_REQUEST *structRequest);
 
+/*
+ * Takes a FILE, socket and fileSize as arguments
+ *
+ * Reads the file in chunks of 1500 bytes and writes it to the socket
+ *
+ * Returns OK on success, ERROR on failure
+ */
 static int WriteFileToSocket(FILE *fdFile, int sockFd, long iFileSize);
 
+/*
+ * Takes a client socket as parameter
+ *
+ * Runs for each accepted connection.
+ * Handles and parses the requests, and acts accordingly.
+ *
+ * Returns OK on success, and ERROR on failure
+ * */
 int HandleConnection(int sockClientFd);
 
-int BindAndListen();
 
 struct URL *ParseURL(char *szUrl);
 
+/*
+ * Takes the client socket and a string buffer as params.
+ *
+ * Reads one line of the request and sets the buffer with it
+ *
+ * Returns OK
+ * */
 static int ReadLine(int sockFd, char *szLineBuffer);
 
-static int *GetHeaders(int sockFd, HTTP_REQUEST *structRequest);
-
+/*
+ * Takes an HTTP_REQUEST as the first param and a socket file descriptor as the second param
+ *
+ * Parse lines for "key: val" and sets the HTTP_REQUEST struct accordingly
+ *
+ * Returns OK
+ */
 static int SplitHeaders(HTTP_REQUEST *structHttpRequest, int sockFd);
 
-static int GetFile(char *szFilePath, char *szFileBuffer);
+/*
+ * Takes a client socket and a HTTP_RESPONSE as params
+ *
+ * Sends HTTP headers to the client socket
+ *
+ * Returns OK on success, and ERROR on failure
+ * */
+static int SendResponseHeaders(int sockFd, HTTP_RESPONSE *structResponse);
 
+/*
+ * Splits the string on the "." delimiter, and checks if the
+ * file extension matches any known ones.
+ * Returns an enum in the range of 0 - 6, where 6 is UNKNOWN.
+ *
+ * On error, it returns -1.
+ */
 static int GetFileExtension(char *szFileName);
 
+/*
+ * Set the header fields
+ * in the HTTP_RESPONSE struct
+ */
 static int SetResponseHeaders(HTTP_REQUEST *structRequest, HTTP_RESPONSE *structResponse, long iFileSize);
 #endif //C_PROG_HTTP_UTILS_H
--- oppgave_5/server/include/server.h
+++ oppgave_5/server/include/server.h
@@ -1,6 +1,19 @@
 #ifndef __CPROG_CLIENT
 #define __CPROG_CLIENT
 
-int ConnectToSocket(char *szHost, int sockFd);
+/*
+ * Binds and listens to socket.
+ *
+ * Returns a socket file descriptor on success, -1 on failure.
+ */
+int BindAndListen();
+/*
+ * Fires when user hits CTRL+C to exit. Closes the socket and sets iRunning to 0.
+ */
+void SIGINTHandler();
+/*
+ * Fires on error. Closes the socket and sets iRunning to 0.
+ */
+void SIGABRTHandler();
 
 #endif
